4c76e383677f4fc5c942d135c3d78d140ec96059
910 <240450828@qq.com>
2019-09-11 20:26:55 +0800

    mian io input

--- testio/test.py
+++ testio/test.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-with open('./in.txt', 'r') as f:
-    data = f.readlines()
+# with open('./in.txt', 'r') as f:
+#     data = f.readlines()
 
-    team = []
-    for line in data:
-        # print(line)
-        item = line.split()
-        # item = map(int item)
-        item = [int(i) for i in item]
-        team.append(item)
-print(team)
+#     team = []
+#     for line in data:
+#         # print(line)
+#         item = line.split()
+#         # item = map(int item)
+#         item = [int(i) for i in item]
+#         team.append(item)
+# print(team)
 
 # while True:
 #     line = stdin.readline().strip()
@@ -19,3 +19,16 @@ print(team)
 #     item = line.split()
 #     item = [int(i) for i in item]
 #     team.append(item)
+
+
+
+while 1:
+    nm = list(map(int, input() .split(' ')) )
+    n = nm[0]
+    m = nm[1]
+    A = list(map(int, input().split(' ')))
+    B = list(map(int, input().split(' ')))
+    res = []
+    res = set(A+B)
+    print(' '.join(str(i) for i in sorted(res)))
+          
