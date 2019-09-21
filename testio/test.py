125b512dfc81a5ea90eb8452d81bd61a721edc02
910 <240450828@qq.com>
2019-09-21 16:48:23 +0800

    add testio read file to stdin

--- testio/test.py
+++ testio/test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from sys import stdin
 # with open('./in.txt', 'r') as f:
 #     data = f.readlines()
 
@@ -11,22 +11,24 @@
 #         item = [int(i) for i in item]
 #         team.append(item)
 # print(team)
+team = []
+stdin =  open('./in.txt', 'r')
+while True:
+    line = stdin.readline().strip()
+    if line=='':
+        break
+    item = line.split()
+    item = [int(i) for i in item]
+    team.append(item)
+print(team)
 
-# while True:
-#     line = stdin.readline().strip()
-#     if line=='':
-#         break
-#     item = line.split()
-#     item = [int(i) for i in item]
-#     team.append(item)
 
 
-
-while 1:
-    nm = list(map(int, input().split(' ')) )
-    n = nm[0]
-    m = nm[1]
-    A = list(map(int, input().split(' ')))
-    B = list(map(int, input().split(' ')))
-    print(' '.join(str(i) for i in sorted(set(A+B))))
+# while 1:
+#     nm = list(map(int, input().split(' ')) )
+#     n = nm[0]
+#     m = nm[1]
+#     A = list(map(int, input().split(' ')))
+#     B = list(map(int, input().split(' ')))
+#     print(' '.join(str(i) for i in sorted(set(A+B))))
           
